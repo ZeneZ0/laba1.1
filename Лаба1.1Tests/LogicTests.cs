cae7d4a37cb9735722ad11e7aeafb43de32cc1a1
stud <stud@I21013406101>
2025-02-18 12:40:25 +0800

    tets commit

--- Лаба1.1Tests/LogicTests.cs
+++ Лаба1.1Tests/LogicTests.cs
@@ -8,13 +8,120 @@ using System.Threading.Tasks;
 
 namespace Zad12.Tests
 {
-    [TestClass()]
+    [TestClass]
     public class LogicTests
     {
-        [TestMethod()]
-        public void ConvertKopecksToRublesTest()
+        [TestMethod]
+        public void ConvertKopecksToRubles_321_ReturnsCorrectString()
         {
-            Assert.Fail();
+            
+            int kopecks = 321;
+            string expected = "3 рубля 21 копейка";
+
+            
+            string result = Logic.ConvertKopecksToRubles(kopecks);
+
+            
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ConvertKopecksToRubles_1505_ReturnsCorrectString()
+        {
+            
+            int kopecks = 1505;
+            string expected = "15 рублей 5 копеек";
+
+            
+            string result = Logic.ConvertKopecksToRubles(kopecks);
+
+           
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ConvertKopecksToRubles_100_ReturnsCorrectString()
+        {
+            
+            int kopecks = 100;
+            string expected = "1 рубль";
+
+            
+            string result = Logic.ConvertKopecksToRubles(kopecks);
+
+            
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ConvertKopecksToRubles_0_ReturnsCorrectString()
+        {
+            
+            int kopecks = 0;
+            string expected = "0 рублей 0 копеек";
+
+            
+            string result = Logic.ConvertKopecksToRubles(kopecks);
+
+            
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ConvertKopecksToRubles_50_ReturnsCorrectString()
+        {
+            
+            int kopecks = 50;
+            string expected = "50 копеек";
+
+            
+            string result = Logic.ConvertKopecksToRubles(kopecks);
+
+            
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ConvertKopecksToRubles_5000_ReturnsCorrectString()
+        {
+           
+            int kopecks = 5000;
+            string expected = "50 рублей";
+
+            
+            string result = Logic.ConvertKopecksToRubles(kopecks);
+
+            
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ConvertKopecksToRubles_101_ReturnsCorrectString()
+        {
+           
+            int kopecks = 101;
+            string expected = "1 рубль 1 копейка";
+
+         
+            string result = Logic.ConvertKopecksToRubles(kopecks);
+
+           
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ConvertKopecksToRubles_115_ReturnsCorrectString()
+        {
+            
+            int kopecks = 115;
+            string expected = "1 рубль 15 копеек";
+
+            
+            string result = Logic.ConvertKopecksToRubles(kopecks);
+
+            
+            Assert.AreEqual(expected, result);
         }
     }
+
 }
